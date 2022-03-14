10f9329d9368123df2a48242d2818d5d4b792021
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-14 17:47:22 -0600

    events table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_03_14_225017) do
-=======
-ActiveRecord::Schema.define(version: 2022_03_14_233357) do
->>>>>>> c2d526752401824a2d933ff13cabf73c5005be11
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
