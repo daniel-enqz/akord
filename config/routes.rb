5bc3fa291fc50cdacbe68fee897aae44898fcd24
Edson Quintero <edson_123@me.com>
2022-03-15 14:36:21 -0600

    route Events

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :events, only: %i[show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
