7cb4fc76d71ead5e155c0b65e9373127685b1a7e
Edson Quintero <edson_123@me.com>
2022-03-16 14:12:59 -0600

    daniel

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,12 +2,19 @@ class AttendeesController < ApplicationController
 
   # POST /events/:event_id/attendees
   def create
+    @event = Event.find(params[:event_id])
     @attendee = Attendee.new(attendee_params)
+    if @attendee.save
+      session[:attendee_id] = @attendee.id
+      redirect_to new_event_vote_path(@event)
+    else
+      render "events/join"
+    end
   end
-end
 
-private
+  private
 
-def event_params
-  params.require(:attendee).permit(:name)
+  def attendee_params
+    params.require(:attendee).permit(:name)
+  end
 end
