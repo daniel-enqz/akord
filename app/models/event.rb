f206d5481954d110a2d5cbd6ac64f7a1c12711a7
Alejandro Luna <alunag95@gmail.com>
2022-03-17 13:10:53 -0600

    added fun id

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :votes
 
+  before_create :generate_fun_id
+
   attribute :title, :string, default: "Our Event"
 
   def event_dates
@@ -18,5 +20,13 @@ class Event < ApplicationRecord
   def votable_dates_strings=(value)
     value = value.split(", ") if value.is_a? String
     super(value)
+  end
+
+  private
+
+  def generate_fun_id
+    nouns = %w[goose pancake pie gorilla memory shirt pickle fruit beast house]
+    adjectives = %w[pretty funky smelly dirty silly tiny kind huge lovely delighful sweet]
+    self.funid = "#{adjectives.sample}-#{nouns.sample}"
   end
 end
