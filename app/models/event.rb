d91eb53fc3ea959f06bac10c1fe9e920f57c7212
Edson Quintero <edson_123@me.com>
2022-03-17 14:13:22 -0600

    validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,9 @@ class Event < ApplicationRecord
   has_many :votes
 
   attribute :title, :string, default: "Our Event"
-  validates :title, :description, presence: true
+  validates :title, presence: true
+
+  validate :votable_date_before_today
 
   def event_dates
     votable_dates_strings.map { |date_string| Event::Date.new(date_string, self) }
@@ -16,8 +18,23 @@ class Event < ApplicationRecord
   end
 
   # Might help with flatpick values if they are JSON
+  # "2020-10-10, 2020-10-11" => ["2020-10-10", "2020-10-11"]
   def votable_dates_strings=(value)
     value = value.split(", ") if value.is_a? String
     super(value)
+  end
+
+  def votable_dates
+    votable_dates_strings.map { |ds| ::Date.parse(ds) }
+  end
+
+  private
+
+  def votable_date_before_today
+    errors.add(:votable_dates_strings, "Cannot select past dates") if any_past_dates?
+  end
+
+  def any_past_dates?
+    votable_dates.any? { |date| date < ::Date.current }
   end
 end
