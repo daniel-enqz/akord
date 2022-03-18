50a7b55b541631cf928943a9d69726efa650bc28
Alejandro Luna <alunag95@gmail.com>
2022-03-18 15:17:55 -0600

    connected stimulus controller to event show, sibscrbed each date vote and broadcasted the new vote as a hash with date and rate into each date

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -6,6 +6,43 @@ export default class extends Controller {
   static targets = ["dates"]
 
   connect() {
-    console.log(`Subscribe to the chatroom with the id ${this.eventIdValue}.`)
+    this.channel = consumer.subscriptions.create(
+      { channel: "EventChannel", id: this.eventIdValue },
+      {
+        received: data => this.#insertDatesVotes(data)
+      }
+    )
+    console.log(`Subscribed to the chatroom with the id ${this.eventIdValue}.`)
+  }
+
+  #insertDatesVotes(data) {
+    console.log("Received")
+    data.forEach((voteData) => {
+      const element = this.#dateElements(voteData.date)
+      this.#castVoteFor(element, voteData.rate)
+    })
+  }
+
+  #dateElements(date_string) {
+    const element = this.datesTarget.querySelector(`[data-date='${date_string}']`)
+    return {
+      noDisplay: element.querySelector(".no-display"),
+      yesDisplay: element.querySelector(".yes-display"),
+      totalDisplay: element.querySelector(".total-display"),
+      progressBar: element.querySelector(".progress-bar")
+    }
+  }
+
+  #castVoteFor(element, voteString) {
+    const voteDisplay = element[`${voteString}Display`]
+    const totalDisplay = element.totalDisplay
+    if (voteDisplay) {
+      voteDisplay.innerText = parseInt(voteDisplay.innerText, 10) + 1
+    }
+    totalDisplay.innerText = parseInt(totalDisplay.innerText, 10) + 1
+
+    const yesCount = parseInt(element.yesDisplay.innerText, 10)
+    const progressBarValue = (yesCount * 100) / parseInt(totalDisplay.innerText, 10)
+    element.progressBar.style.width = `${progressBarValue}%`
   }
 }
