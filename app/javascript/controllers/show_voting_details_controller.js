9f57c93928ad31c69547af1925e550f047fad7d0
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-18 13:12:43 -0600

    Details updated in showpage

--- app/javascript/controllers/show_voting_details_controller.js
+++ app/javascript/controllers/show_voting_details_controller.js
@@ -1,17 +1,12 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["details", "voters"]
+  static targets = [ "voters"]
 
   connect() {
     console.log("Hello");
   }
-  displayDetails() {
-    this.detailsTarget.classList.toggle("d-none")
-    this.votersTarget.classList.add("d-none")
-  }
   displayVoters() {
     this.votersTarget.classList.toggle("d-none")
-    this.detailsTarget.classList.add("d-none")
   }
 }
