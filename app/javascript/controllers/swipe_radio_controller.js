1c8b8694a3dc26391981f22f94a75e662ae6801c
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-21 17:04:55 -0600

    Swipe Animation added

--- app/javascript/controllers/swipe_radio_controller.js
+++ app/javascript/controllers/swipe_radio_controller.js
@@ -3,14 +3,13 @@ import Hammer from "hammerjs"
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["input"]
+  static targets = ["input", "thumbsup", "thumbsdown", "icons"]
 
   connect() {
-    debugger
     this.initialX = this.element.getBoundingClientRect().x
-    this.threshold = 100
+    this.threshold = 50
     this.inputValues = { yes: 1, no: -1, maybe: 0 }
-    this.inputFinalPositions = { yes: 100, no: -100, maybe: 0 }
+    this.inputFinalPositions = { yes: 50, no: -50, maybe: 0 }
     this.inputValueClasses = ["swipe-no", "swipe-maybe", "swipe-yes"]
     this.hammerSquare = new Hammer(this.element)
     this.hammerSquare.on('panstart', (e) => this.#panStart(e))
@@ -21,6 +20,9 @@ export default class extends Controller {
     this.currentX = this.element.getBoundingClientRect().x
     this.hammerSquare.on('panmove', (e) => this.#panMove(e))
     this.hammerSquare.on('panend', (e) => this.#panEnd(e))
+
+    this.iconsTarget.classList.remove("punch-in")
+    this.iconsTarget.classList.add("d-none")
   }
 
   get currentLeft() {
@@ -48,9 +50,25 @@ export default class extends Controller {
     this.element.classList.add(this.currentClass)
   }
 
+  #displayIcon() {
+    if (this.currentValue == "yes") {
+      this.thumbsupTarget.classList.remove("d-none")
+      this.thumbsdownTarget.classList.add("d-none")
+    } else if (this.currentValue == "no") {
+      this.thumbsupTarget.classList.add("d-none")
+      this.thumbsdownTarget.classList.remove("d-none")
+    } else {
+      this.thumbsupTarget.classList.add("d-none")
+      this.thumbsdownTarget.classList.add("d-none")
+    }
+  }
+
   #panEnd(e) {
     this.element.classList.add("smooth-transition")
     this.inputTarget.value = this.inputValues[this.currentValue]
+    this.#displayIcon()
+    this.iconsTarget.classList.add("punch-in")
+    this.iconsTarget.classList.remove("d-none")
 
     this.element.style.left = `${this.inputFinalPositions[this.currentValue]}px`
 
