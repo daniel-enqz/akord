2f5d587d53ab52ef21d0c4cb215a08d936d982c3
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-22 15:26:43 -0600

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
 # Users Generation
-default_password = "123456"
-5.times do |n|
-  puts "Creating User....."
-  user = User.create!(email: "user#{n}@user.com", password: default_password,
-                      name: Faker::Name.first_name)
-  valid_rates = Vote.rates.keys
-  next_three_days = [Date.current, Date.current + 1, Date.current + 2]
+# default_password = "123456"
+# 5.times do |n|
+#   puts "Creating User....."
+#   user = User.create!(email: "user#{n}@user.com", password: default_password,
+#                       name: Faker::Name.first_name)
+#   valid_rates = Vote.rates.keys
+#   next_three_days = [Date.current, Date.current + 1, Date.current + 2]
 
-  puts "Creating Event....."
-  event = Event.create!(
-    title: "Le Wagon - Assado",
-    description: "Assado at Le Wagon",
-    user: user,
-    votable_dates_strings: next_three_days
-  )
+#   puts "Creating Event....."
+#   event = Event.create!(
+#     title: "Le Wagon - Assado",
+#     description: "Assado at Le Wagon",
+#     user: user,
+#     votable_dates_strings: next_three_days
+#   )
 
-  puts "Creating Votes....."
-  10.times do
-    next_three_days.each do |date|
-      event.votes.create!(
-        date: date,
-        rate: valid_rates.sample,
-        attendee: Attendee.new(name: Faker::Name.name)
-      )
-    end
-  end
-end
+#   puts "Creating Votes....."
+#   10.times do
+#     next_three_days.each do |date|
+#       event.votes.create!(
+#         date: date,
+#         rate: valid_rates.sample,
+#         attendee: Attendee.new(name: Faker::Name.name)
+#       )
+#     end
+#   end
+# end
