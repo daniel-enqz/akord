74b12b89dc008fe9d1457d2da75456cf2f60ca1e
Daniel Enriquez <daniel.enm17@gmail.com>
2022-03-24 10:04:31 -0600

    adding calendar

--- startredis.sh
+++ startredis.sh
@@ -1,8 +1,8 @@
+#!/bin/sh
+
 redis-cli shutdown
 echo "Previous redis-server closed✅"
 read -p 'ENTER to create redis-server---> ' upstreamVar
 
 redis-server
 echo "New redis-server open✅"
-
-x-terminal-emulator
